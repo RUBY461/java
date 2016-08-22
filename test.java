26b6a7d6a9cf2cd9ca30e5062f7eb560b99feb50
Ruby461 <ruyishi.ruby@gmail.com>
2016-08-22 11:40:08 -0400

    build test

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+
+
+public static void main(String[] args) {
+	System.out.println(exchange("Skywalker","Aya"));
+	}
+
+static String exchange(String first, String last){
+	return last+" "+first;
+	}
+}
